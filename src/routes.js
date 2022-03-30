79ff3b8f22627b614404e1605d38857dd3415d5c
Diego Maustem <diego.maustem@gmail.com>
2022-03-30 11:07:52 -0300

    pagina-not-found

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import Home from './pages/Home';
 import Filme from './pages/Filme';
 import Favoritos from './pages/Favoritos';
+import Error from './pages/Error';
 
 const Routes = () => {
     return(
@@ -12,6 +13,7 @@ const Routes = () => {
                 <Route exact path="/" component={Home}/>
                 <Route exact path="/filme/:id" component={Filme}/>
                 <Route exact path="/favoritos" component={Favoritos}/>
+                <Route exact path="*" component={Error}/>
             </Switch>
         </BrowserRouter>
     )
